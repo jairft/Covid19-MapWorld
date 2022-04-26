ba2db4575006ae647d4bd1440119aaf7f27d2bc3
jairft <jairpg1993@gmail.com>
2022-04-26 11:30:20 -0300

    map-covid19-world

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,56 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import SpainFlag from '../../assets/images/Spain.png'
+import ArgentinaFlag from '../../assets/images/argentina.png'
+import JapanFlag from '../../assets/images/japao.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Espanha',
+    value: 'spain',
+    flag: SpainFlag
+
+  },
+  {
+    label: 'Argentina',
+    value: 'argentina',
+    flag: ArgentinaFlag
+
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+    flag: JapanFlag
+
+  }
+]
+
+export default COUNTRIES
